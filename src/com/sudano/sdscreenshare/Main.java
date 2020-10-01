1aa1a847183f0a362225f858fd15de58c458c5dd
crlshnrrq <caarlosheenrique01@outlook.com>
2020-10-01 18:38:14 -0300

    Adicionado lista para salvar as screenshares

--- src/com/sudano/sdscreenshare/Main.java
+++ src/com/sudano/sdscreenshare/Main.java
@@ -1,13 +1,13 @@
 package com.sudano.sdscreenshare;
 
-import java.util.List;
+import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.sudano.sdscreenshare.comandos.ScreenShare;
+import com.sudano.sdscreenshare.comandos.ScreenShareCmd;
 import com.sudano.sdscreenshare.eventos.EventosSS;
 
 public class Main extends JavaPlugin {
@@ -17,12 +17,27 @@ public class Main extends JavaPlugin {
 
 	public static String mundo_nome;
 	public static String mundoss_nome;
-	
+
 	public static String retirado_ss;
 	public static String colocado_ss;
-	
+
 	public static String comandoban;
 	public static String banirquit;
+
+	private static final ArrayList<ScreenShare> screenshares = new ArrayList<>();
+
+	public static ArrayList<ScreenShare> getScreenshares() {
+		return screenshares;
+	}
+
+	public static ScreenShare getScreenShareByID(int id) {
+		return getScreenshares().stream().filter(ss -> ss.getID() == id).findFirst().orElse(null);
+	}
+
+	public static ScreenShare getScreenShareBySuspect(String nickname) {
+		return getScreenshares().stream().filter(ss -> ss.getSuspect().equalsIgnoreCase(nickname)).findFirst()
+				.orElse(null);
+	}
 
 	public static Main getInstace() {
 		return instance;
@@ -39,7 +54,7 @@ public class Main extends JavaPlugin {
 		mundo_nome = this.getConfig().getString("Mundo");
 		mundoss_nome = this.getConfig().getString("MundoSS");
 		banirquit = this.getConfig().getString("BanirDesconectar");
-		
+
 		Bukkit.getConsoleSender().sendMessage("[sdScreenShare] - Plugin §ahabilitado§f.");
 
 		World w_mundoss = Bukkit.getWorld(mundoss_nome);
@@ -57,13 +72,12 @@ public class Main extends JavaPlugin {
 			Bukkit.getConsoleSender()
 					.sendMessage("[sdScreenShare] - §c§lERRO! §cArrume o nome dos mundos no arquivo §lplugin.yml§c!");
 		}
-		
-		getCommand("ss").setExecutor(new ScreenShare());
-		getCommand("screenshare").setExecutor(new ScreenShare());
-		
+
+		getCommand("ss").setExecutor(new ScreenShareCmd());
+		getCommand("screenshare").setExecutor(new ScreenShareCmd());
+
 		Bukkit.getPluginManager().registerEvents(new EventosSS(), this);
 	}
-	
 
 	public void onDisable() {
 		Bukkit.getConsoleSender().sendMessage("[sdScreenShare] - Plugin §cdesabilitado§f.");
@@ -80,5 +94,4 @@ public class Main extends JavaPlugin {
 	public static Plugin instance() {
 		return plugin;
 	}
-
 }
