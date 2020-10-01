514b5b67aa0e80b68ec6aa3d7dc0b953ad812d2f
crlshnrrq <caarlosheenrique01@outlook.com>
2020-10-01 18:38:37 -0300

    Aplicando o uso da api na classe de eventos

--- src/com/sudano/sdscreenshare/eventos/EventosSS.java
+++ src/com/sudano/sdscreenshare/eventos/EventosSS.java
@@ -8,40 +8,38 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.sudano.sdscreenshare.Main;
-import com.sudano.sdscreenshare.comandos.ScreenShare;
+import com.sudano.sdscreenshare.ScreenShareAPI;
 
 public class EventosSS implements Listener {
 
 	@EventHandler
 	public void onQuit(PlayerQuitEvent e) {
 		Player p = e.getPlayer();
-		if (ScreenShare.screenshare.contains(p)) {
-			ScreenShare.screenshare.remove(p);
+		if (ScreenShareAPI.hasScreenShare(p)) {
 			for (Player todos : Bukkit.getOnlinePlayers()) {
 				for (String s : Main.plugin.getConfig().getStringList("DesconectouSS")) {
 					todos.sendMessage(s.replace("&", "§").replace("{player}", p.getName()));
 				}
 			}
 			if (Boolean.valueOf(Main.banirquit) == true) {
-				Main.comandoban = Main.getPlugin().getConfig().getString("ComandoBan").replace("/", "").replace("{player}", p.getName());
+				Main.comandoban = Main.getPlugin().getConfig().getString("ComandoBan").replace("/", "")
+						.replace("{player}", p.getName());
 				Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), Main.comandoban);
+			}
+			ScreenShareAPI.removeScreenShare(p);
+		}
+	}
+
+	@EventHandler
+	public void onTryEscape(PlayerCommandPreprocessEvent e) {
+		Player p = e.getPlayer();
+		if (ScreenShareAPI.hasScreenShare(p)) {
+			String cmd = e.getMessage().toLowerCase();
+			if (cmd.startsWith("/")) {
+				e.setCancelled(true);
+				p.sendMessage("§cVocê não pode executar comandos na ScreenShare!");
 			}
 
 		}
 	}
-	
-	@EventHandler
-	public void onTryEscape(PlayerCommandPreprocessEvent e) {
-		Player p = e.getPlayer();
-		if(ScreenShare.screenshare.contains(p)) {
-		String cmd = e.getMessage().toLowerCase();
-		if(cmd.startsWith("/")) {
-			e.setCancelled(true);
-			p.sendMessage("§cVocê não pode executar comandos na ScreenShare!");
-		}
-		
-	}
-	}
-	
-
 }
