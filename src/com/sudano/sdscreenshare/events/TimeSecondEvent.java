cbda95b13eabb682047a054396bd3f5962c81485
crlshnrrq <caarlosheenrique01@outlook.com>
2020-10-03 22:43:31 -0300

    Adicionado evento para manipular os segundos

---
+++ src/com/sudano/sdscreenshare/events/TimeSecondEvent.java
@@ -0,0 +1,18 @@
+package com.sudano.sdscreenshare.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public final class TimeSecondEvent extends Event {
+
+	private static final HandlerList handlers = new HandlerList();
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
