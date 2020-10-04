0e8649ba24c0ad564d2fc11c17085058b510f6b7
crlshnrrq <caarlosheenrique01@outlook.com>
2020-10-03 22:43:09 -0300

    Criando superclasse para a classe Main

--- src/com/sudano/sdscreenshare/Main.java
+++ src/com/sudano/sdscreenshare/Main.java
@@ -1,97 +1,31 @@
 package com.sudano.sdscreenshare;
 
-import java.util.ArrayList;
-
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.sudano.sdscreenshare.comandos.ScreenShareCmd;
-import com.sudano.sdscreenshare.eventos.EventosSS;
+public final class Main extends JavaPlugin {
 
-public class Main extends JavaPlugin {
+	private static ScreenSharePlugin screenSharePlugin;
 
-	public static Plugin plugin;
-	public static Main instance;
-
-	public static String mundo_nome;
-	public static String mundoss_nome;
-
-	public static String retirado_ss;
-	public static String colocado_ss;
-
-	public static String comandoban;
-	public static String banirquit;
-
-	private static final ArrayList<ScreenShare> screenshares = new ArrayList<>();
-
-	public static ArrayList<ScreenShare> getScreenshares() {
-		return screenshares;
+	public static ScreenSharePlugin getScreenSharePlugin() {
+		return screenSharePlugin;
 	}
 
-	public static ScreenShare getScreenShareByID(int id) {
-		return getScreenshares().stream().filter(ss -> ss.getID() == id).findFirst().orElse(null);
+	@Override
+	public void onLoad() {
+		super.onLoad();
+		screenSharePlugin = new ScreenSharePlugin(this);
+		getScreenSharePlugin().onLoad();
 	}
 
-	public static ScreenShare getScreenShareBySuspect(String nickname) {
-		return getScreenshares().stream().filter(ss -> ss.getSuspect().equalsIgnoreCase(nickname)).findFirst()
-				.orElse(null);
-	}
-
-	public static Main getInstace() {
-		return instance;
-	}
-
+	@Override
 	public void onEnable() {
-		plugin = this;
-		instance = this;
-		try {
-			this.saveDefaultConfig();
-		} catch (Exception var1_1) {
-		}
-
-		mundo_nome = this.getConfig().getString("Mundo");
-		mundoss_nome = this.getConfig().getString("MundoSS");
-		banirquit = this.getConfig().getString("BanirDesconectar");
-
-		Bukkit.getConsoleSender().sendMessage("[sdScreenShare] - Plugin §ahabilitado§f.");
-
-		World w_mundoss = Bukkit.getWorld(mundoss_nome);
-		if (w_mundoss == null) {
-			Bukkit.getConsoleSender().sendMessage(
-					"[sdScreenShare] - §c§lERRO! §cNao foi possivel encontrar o mundo §f\"" + mundoss_nome + "§c\"");
-			Bukkit.getConsoleSender()
-					.sendMessage("[sdScreenShare] - §c§lERRO! §cArrume o nome dos mundos no arquivo §lplugin.yml§c!");
-		}
-
-		World w_mundo = Bukkit.getWorld(mundo_nome);
-		if (w_mundo == null) {
-			Bukkit.getConsoleSender().sendMessage(
-					"[sdScreenShare] - §c§lERRO! §cNao foi possivel encontrar o mundo §f\"" + mundo_nome + "§c\"");
-			Bukkit.getConsoleSender()
-					.sendMessage("[sdScreenShare] - §c§lERRO! §cArrume o nome dos mundos no arquivo §lplugin.yml§c!");
-		}
-
-		getCommand("ss").setExecutor(new ScreenShareCmd());
-		getCommand("screenshare").setExecutor(new ScreenShareCmd());
-
-		Bukkit.getPluginManager().registerEvents(new EventosSS(), this);
+		super.onEnable();
+		getScreenSharePlugin().onEnable();
 	}
 
+	@Override
 	public void onDisable() {
-		Bukkit.getConsoleSender().sendMessage("[sdScreenShare] - Plugin §cdesabilitado§f.");
-	}
-
-	public static Plugin getPlugin() {
-		return plugin;
-	}
-
-	public static Plugin getInstance() {
-		return plugin;
-	}
-
-	public static Plugin instance() {
-		return plugin;
+		super.onDisable();
+		getScreenSharePlugin().onDisable();
 	}
 }
---
+++ src/com/sudano/sdscreenshare/ScreenSharePlugin.java
@@ -0,0 +1,72 @@
+package com.sudano.sdscreenshare;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.WorldCreator;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sudano.sdscreenshare.events.TimeSecondEvent;
+import com.sudano.sdscreenshare.guis.ScreenShareInfoGUI;
+import com.sudano.sdscreenshare.guis.ScreenShareSessionsGUI;
+
+public final class ScreenSharePlugin {
+
+	private final JavaPlugin plugin;
+
+	public ScreenSharePlugin(JavaPlugin plugin) {
+		this.plugin = plugin;
+	}
+
+	public JavaPlugin getPlugin() {
+		return this.plugin;
+	}
+
+	private static final ArrayList<ScreenShare> screenshares = new ArrayList<>();
+
+	public static ArrayList<ScreenShare> getScreenshares() {
+		return screenshares;
+	}
+
+	public static ScreenShare getScreenShareByID(int id) {
+		return getScreenshares().stream().filter(ss -> ss.getID() == id).findFirst().orElse(null);
+	}
+
+	public static ScreenShare getScreenShareBySuspect(String nickname) {
+		return getScreenshares().stream().filter(ss -> ss.getSuspect().equalsIgnoreCase(nickname)).findFirst()
+				.orElse(null);
+	}
+
+	public static Location getScreenShareLocation() {
+		return Bukkit.getWorld("screenshare_world").getSpawnLocation();
+	}
+
+	public static Location getDefaultWorldLocation() {
+		return Bukkit.getWorld("world").getSpawnLocation();
+	}
+
+	public void onLoad() {
+		new WorldCreator("screenshare_world").createWorld();
+	}
+
+	public void onEnable() {
+		PluginManager pm = Bukkit.getPluginManager();
+		pm.registerEvents(new ScreenShareSessionsGUI(), this.getPlugin());
+		pm.registerEvents(new ScreenShareInfoGUI(), this.getPlugin());
+
+		pm.registerEvents(new ScreenShareListeners(), this.getPlugin());
+
+		this.getPlugin().getCommand("screenshare").setExecutor(new ScreenShareCommand());
+
+		Bukkit.getScheduler().runTaskTimer(this.getPlugin(),
+				() -> Bukkit.getPluginManager().callEvent(new TimeSecondEvent()), 20L, 20L);
+	}
+
+	public void onDisable() {
+		HandlerList.unregisterAll(this.getPlugin());
+		Bukkit.getScheduler().cancelTasks(this.getPlugin());
+	}
+}
