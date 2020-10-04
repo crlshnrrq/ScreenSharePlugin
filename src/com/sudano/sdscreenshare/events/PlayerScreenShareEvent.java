030713c18cb08dd30a1d389d6c85a3c0bb445788
crlshnrrq <caarlosheenrique01@outlook.com>
2020-10-03 22:46:22 -0300

    Adicionado classe que representa eventos relacionados a ScreenShare

---
+++ src/com/sudano/sdscreenshare/events/PlayerScreenShareEvent.java
@@ -0,0 +1,48 @@
+package com.sudano.sdscreenshare.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+import com.sudano.sdscreenshare.ScreenShare;
+
+public class PlayerScreenShareEvent extends PlayerEvent implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+	private ScreenShare screenShare;
+	private boolean cancelled;
+
+	public PlayerScreenShareEvent(Player player, ScreenShare screenShare) {
+		super(player);
+		this.screenShare = screenShare;
+		this.cancelled = false;
+	}
+
+	public ScreenShare getScreenShare() {
+		return this.screenShare;
+	}
+
+	public void setScreenShare(ScreenShare screenShare) {
+		this.screenShare = screenShare;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return this.cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancel) {
+		this.cancelled = cancel;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
