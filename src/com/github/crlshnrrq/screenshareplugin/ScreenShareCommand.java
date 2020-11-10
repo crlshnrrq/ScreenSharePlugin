8383f4a7ccb1ec63f52ec6f36853a34b1b816572
crlshnrrq <caarlosheenrique01@outlook.com>
2020-11-10 11:00:25 -0300

    Referenciando as mensagens ao arquivo messages.yml

--- src/com/github/crlshnrrq/screenshareplugin/ScreenShareCommand.java
+++ src/com/github/crlshnrrq/screenshareplugin/ScreenShareCommand.java
@@ -9,51 +9,39 @@ import org.bukkit.entity.Player;
 import com.github.crlshnrrq.screenshareplugin.guis.ScreenShareHistoryGUI;
 import com.github.crlshnrrq.screenshareplugin.guis.ScreenSharePlayerGUI;
 import com.github.crlshnrrq.screenshareplugin.guis.ScreenShareSessionsGUI;
+import com.github.crlshnrrq.screenshareplugin.guis.config.ScreenShareConfigGUI;
 
 public final class ScreenShareCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		ScreenShareConfig config = ScreenSharePlugin.getConfig();
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
-			if (player.hasPermission("sdscreenshare.use")) {
+			if (player.hasPermission(config.getCommand_Permission())) {
 				if (args.length > 0) {
 					if (args[0].equalsIgnoreCase("-sessions")) {
 						ScreenShareSessionsGUI.openGUI(player);
-						player.sendMessage("§aVocê abriu a Lista de ScreenShare's!");
+						player.sendMessage(config.getCommand_OpenSessions());
 					} else if (args[0].equalsIgnoreCase("-history")) {
-						ScreenShareHistoryGUI.openGUI(player);
-						player.sendMessage("§aVocê abriu o Histórico de Sessões!");
-					} else if (args[0].equalsIgnoreCase("-spectate")) {
-						if (args.length > 1) {
-							Player target = Bukkit.getPlayer(args[1]);
-							if (target != null) {
-								if (ScreenShareAPI.hasScreenShare(target)) {
-									ScreenShare ss = ScreenShareAPI.getScreenShare(target);
-									if (!ss.getAuthor().equalsIgnoreCase(player.getName())) {
-										if (!ScreenShareAPI.hasScreenShare(player) || ScreenShareAPI
-												.getScreenShare(player).getSuspect() != target.getName())
-											ScreenShareAPI.joinScreenShare(player, ss);
-										else
-											ScreenShareAPI.quitScreenShare(player, ss);
-									} else
-										player.sendMessage("§cO Autor da ScreenShare não pode sair!");
-								} else
-									player.sendMessage("§c" + target.getName() + " não está em uma ScreenShare!");
-							} else
-								player.sendMessage("§c" + args[1] + " não foi encontrado!");
+						if (player.hasPermission(config.getCommand_ViewHistoryPermission())) {
+							ScreenShareHistoryGUI.openGUI(player);
+							player.sendMessage(config.getCommand_OpenHistorySessions());
 						} else
-							player.sendMessage("§cSintaxe incorreta, use: /" + label + " -spectate (Jogador)");
+							player.sendMessage(config.getCommand_InsuficientPermissions());
+					} else if (args[0].equalsIgnoreCase("-config")) {
+						ScreenShareConfigGUI.openGUI(player);
+						player.sendMessage(config.getCommand_OpenConfig());
 					} else {
 						ScreenSharePlayerGUI.openGUI(player, args[0]);
-						player.sendMessage("§aVocê abriu as Informações de " + args[0] + "!");
+						player.sendMessage(config.getCommand_OpenPlayerInfo(args[0]));
 					}
 				} else
-					player.sendMessage("§cSintaxe incorreta, use: /" + label + " (Jogador)");
+					player.sendMessage(config.getCommand_Usage(label));
 			} else
-				player.sendMessage("§cVocê não possui Permissão para usar este Comando!");
+				player.sendMessage(config.getCommand_InsuficientPermissions());
 		} else
-			sender.sendMessage("§cEste Comando só pode ser utilizado por um Jogador!");
+			sender.sendMessage(config.getCommand_OnlyPlayersUse());
 		return true;
 	}
 }
